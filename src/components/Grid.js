8b046656b134ed455e759ae860f8fb2e4ea8fbf2
john <john@spinningplates.org>
2020-08-19 22:28:57 -0700

    Display 25x25 grid layout

---
+++ src/components/Grid.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const row = 25;
+const column = 25;
+const val = 0;
+
+const buildGrid = (row, column, val) =>
+  new Array(row).fill(null).map((v) => new Array(column).fill(val));
+
+const grid25 = buildGrid(row, column, val);
+
+const Grid = () => {
+  const [grid, setGrid] = useState(grid25);
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${column}, 20px)`,
+      }}
+    >
+      {grid.map((rows, x) =>
+        rows.map((col, y) => (
+          <div
+            key={`${x}-${y}`}
+            style={{ width: 20, height: 20, border: '1px solid black' }}
+          >
+            {col}
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Grid;
